e875c3b968e3def8b4a3f6a086153209d8644cea
SabriMourelle <89493758+SabriMourelle@users.noreply.github.com>
2022-09-07 00:27:35 -0300

    EjRepaso3
    Ejercicio de repaso número 3 completo

---
+++ EjRepaso3/EjRepaso3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjRepaso3
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+             bool Palindromo(string pali)
+            {
+                return pali.SequenceEqual(pali.Reverse());
+            }
+            //Ejercicio III
+            //Crear una aplicación que indique, cuando el usuario ingresa una palabra, si ésta es un palíndromo.
+            //Nota: Palíndromo son palabras que al derecho y al revés se leen igual, como "ananá".
+
+            string _palabra = "";
+            bool _palabraRev;
+
+            
+            Console.WriteLine("Ingrese una palabra");
+            _palabra = Console.ReadLine();
+
+            
+            if (Palindromo(_palabra))
+            {
+                Console.WriteLine("La palabra es palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra NO es palíndromo");
+            }
+            
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
